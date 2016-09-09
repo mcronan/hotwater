f8012e7a297b152027bc11ae4422d4596b3e9fa7
Ronan <ronanir600@gmail.com>
2016-09-09 14:19:38 +0100

    new

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
 
-$(".first-container").scroll(function(){
-	// $("first-container:after").css("background-color", "red")
-	console.log('scrolled');
-})
+// $(".first-container").scroll(function(){
+// 	// $("first-container:after").css("background-color", "red")
+// 	console.log('scrolled');
+// })
 
-$('#mc-embedded-subscribe').click(function() {
-	console.log("hello")
-})
+// $('#mc-embedded-subscribe').click(function() {
+// 	console.log("hello")
+// })
 
 
-});
+// });
