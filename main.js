0ed743c917686ad76bf93aff146870bbbc1b0d83
Ronan <ronanir600@gmail.com>
2016-09-10 18:45:06 +0100

    new

--- main.js
+++ main.js
@@ -4,24 +4,56 @@ $(document).ready(function() {
 		var top = $('.first-container').height();
 		var secondTop = $('.first-container').height();
 		
-		if($(this).scrollTop() > 670) {
+		// FIRST CONTAINER
+		// Make head disappear
+		if($(this).scrollTop() > 670 && $(this).scrollTop() < 900) {
 			$('.first-container').css("background-image", "none")
 			console.log("hello");
 		}
 
+		// Make head re-appear
+		if($(this).scrollTop() < 670 || $(this).scrollTop() > 900) {
+			$('.first-container').css("background-image", "url('head-5.png')")
+			console.log("show");
+		}
+
+		// SECOND CONTAINER
+		// Make head disappear
 		if($(this).scrollTop()> 900) {
 			$('.second-container').css("background-image", "none")
 			console.log("hello again");
 		}
 
+		// Make head re-appear
+		if($(this).scrollTop() < 900 || $(this).scrollTop() > 1250) {
+			$('.second-container').css("background-image", "url('head-5.png')")
+			console.log("show");
+		}
+
+		// THIRD CONTAINER
+		// Make head disappear		
 		if($(this).scrollTop()> 1250) {
 			$('.third-container').css("background-image", "none")
 			console.log("hello again");
 		}
 
+		// Make head re-appear
+		if($(this).scrollTop() < 1250 || $(this).scrollTop() > 1450) {
+			$('.third-container').css("background-image", "url('head-5.png')")
+			console.log("show");
+		}
+
+		// FOURTH CONTAINER
+		// Make head disappear
 		if($(this).scrollTop()> 1450) {
 			$('.fourth-container').css("background-image", "none")
 			console.log("hello again");
+		}
+
+		// Make head re-appear
+		if($(this).scrollTop() < 1450) {
+			$('.fourth-container').css("background-image", "url('head-5.png')")
+			console.log("show");
 		}
 	})
 
