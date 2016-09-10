92dc6544c27d204b7072d83df82ebcb2bbcfb43d
Ronan <ronanir600@gmail.com>
2016-09-10 17:59:11 +0100

    new

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
 	
 	$(window).scroll(function() {
-		var Top = $('.first-container').height();
-		if($(this).scrollTop()>Top) {
+		var top = $('.first-container').height();
+		var secondTop = $('.first-container').height();
+		
+		if($(this).scrollTop() > 450) {
 			$('.first-container').css("background-image", "none")
 			console.log("hello");
+		}
+
+		if($(this).scrollTop()> 700) {
+			$('.second-container').css("background-image", "none")
+			console.log("hello again");
+		}
+
+		if($(this).scrollTop()> 950) {
+			$('.third-container').css("background-image", "none")
+			console.log("hello again");
+		}
+
+		if($(this).scrollTop()> 1200) {
+			$('.fourth-container').css("background-image", "none")
+			console.log("hello again");
 		}
 	})
 
